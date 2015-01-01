72279492ac775e5b55e0bd4f09ff6e78c1927117
David Lutton <me@daln.org>
2015-01-01 21:54:05 +0000

    Update makemkv-update.sh

--- makemkv-update.sh
+++ makemkv-update.sh
@@ -1,15 +1,23 @@
-#!/bin/bash
+#!/bin/bash 
 
-Version="1.8.11"
-wget http://www.makemkv.com/download/makemkv-{oss,bin}-$Version.tar.gz
-tar xfvz makemkv-bin-$Version.tar.gz
-tar xfvz makemkv-oss-$Version.tar.gz
+sudo apt-get install checkinstall build-essential libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev libqt4-dev
+version=$( wget "http://www.makemkv.com/download/" -qO- | grep -m 1 "MakeMKV v" | sed -e "s/.*MakeMKV v//;s/ (.*//" )
+# Thanks to [mechevar](http://www.makemkv.com/forum2/memberlist.php?mode=viewprofile&u=5355) for sed`fo [here](http://www.makemkv.com/forum2/viewtopic.php?f=3&t=5266) [#](http://dl.dropbox.com/u/18055299/buildMakeMkv.sh)
 
-cd makemkv-oss-$Version/
-./configure
-make
-sudo make install
-cd ..
-cd makemkv-bin-$Version/
-make
-sudo make install
+for package in "bin" "oss"
+do
+ di=/dev/shm
+ cd ${di}/
+ dir=${di}/makemkv-${package}-${version}
+ file=${dir}.tar.gz
+ if [ ! -e ${file} ]
+ then
+  wget http://www.makemkv.com/download/${file}
+ fi
+ tar xfvz ${file}
+ cd ${dir}
+ ./configure
+ make
+ sudo checkinstall make install
+ rm -rfv ${dir}
+done
