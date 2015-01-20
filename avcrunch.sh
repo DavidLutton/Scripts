f7bfe44f9f03d584f6315ecd54a23dfe45fd8357
David Lutton <me@daln.org>
2015-01-20 21:04:49 +0000

    Use same order as case statement

--- avcrunch.sh
+++ avcrunch.sh
@@ -8,14 +8,15 @@ while IFS= read -r -d $'\0' FILE; do
  case "$PROBE" in
 
   "audio subtitle video ")
-   CONF="$CONF -codec:v h264 -map 0:v"
    CONF="$CONF -codec:a copy -map 0:a:0"
    CONF="$CONF -codec:s copy -map 0:s"
+   CONF="$CONF -codec:v h264 -map 0:v"
+
   ;;
 
   "audio video ")
-   CONF="$CONF -codec:v h264 -map 0:v"
    CONF="$CONF -codec:a copy -map 0:a:0"
+   CONF="$CONF -codec:v h264 -map 0:v"
   ;;
 
   *)
